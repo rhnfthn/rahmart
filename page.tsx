06c75df038fc308032917c2f33dfcc23ece3cdb9
rhnfthn <raihanfathan11@gmail.com>
2026-06-10 00:46:43 +0700

    add ,

--- page.tsx
+++ page.tsx
@@ -1,6 +1,8 @@
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 
+export const dynamic = "force-dynamic";
+
 type Todo = {
   id: number;
   name: string;
